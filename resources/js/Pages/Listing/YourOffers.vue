92b49ed0893fec9cf7e25d21df62288fc493fd2f
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-06-20 23:47:34 -0400

    add new your offer page

---
+++ resources/js/Pages/Listing/YourOffers.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div
+            v-if="offers.length === 0"
+            class="w-full h-[300px] shadow-md rounded p-4 flex justify-center items-center border border-indigo-200"
+        >
+            <h2 class="font-bold">You do not have any offer</h2>
+        </div>
+        <ul v-if="offers.length > 0">
+            <li v-for="(offer, index) in offers" v-bind:key="index">
+                {{ offer.amount }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    offers: Object,
+});
+</script>
