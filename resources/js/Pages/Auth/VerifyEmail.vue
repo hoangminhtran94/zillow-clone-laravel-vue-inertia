8137d9765c95fd4258bbedd4bed4ca965beb643a
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-06-24 22:54:48 -0400

    update verify email screen

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,18 +1,19 @@
 <template>
-    <h1>You need to verify</h1>
-    <div>
+    <PlaceHolderBox>
+        <p class="w-full text-center">You need to verify to acess this page</p>
         <Link
             :href="route(`verification.send`)"
             method="post"
             :data="{ _token: csrf_token }"
             as="button"
-            class="text-indigo-600 dark:text-indigo-400"
+            class="text-indigo-600 dark:text-indigo-400 text-lg text-center w-full hover:scale-110 transition-all"
         >
             Resend verification email</Link
         >
-    </div>
+    </PlaceHolderBox>
 </template>
 <script setup>
 import { Link } from "@inertiajs/vue3";
+import PlaceHolderBox from "../../Components/UI/PlaceHolderBox.vue";
 defineProps({ csrf_token: String });
 </script>
