2dae1c2ea64a6ad2f8574b7735ee8d1c1e409688
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-06-26 02:43:24 -0400

    refactor home page ui

---
+++ resources/js/Components/Home/FeatureItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <Box
+        class-name="p-4 h-[500px] hover:scale-[103%] transition-all cursor-pointer"
+    >
+        <Link :href="route('listing.show', { listing: listing.id })">
+            <div class="h-1/2 w-full">
+                <img
+                    :src="listing.listing_image"
+                    class="w-full h-full object-cover"
+                />
+            </div>
+            <div class="flex flex-col mt-3 gap-1">
+                <h2 class="font-bold">
+                    {{ listing.listing_address }}
+                </h2>
+                <h1>{{ currencyFormater.format(listing.price) }}</h1>
+                <p class="text-slate-400">
+                    {{
+                        new Date(listing.created_at).toLocaleString("en-US", {
+                            dateStyle: "long",
+                            timeStyle: "short",
+                        })
+                    }}
+                </p>
+                <p class="text-slate-400">Listed by {{ listing.owner_name }}</p>
+            </div>
+        </Link>
+    </Box>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import Box from "../UI/Box.vue";
+const currencyFormater = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+});
+defineProps({ listing: Object });
+</script>
---
+++ resources/js/Components/Home/FeatureListings.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-col gap-3">
+        <h1 class="text-center tracking-wider">Features</h1>
+        <div
+            class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-3"
+        >
+            <FeatureItem
+                v-for="listing in listings"
+                :listing="listing"
+                v-bind:key="listing.id"
+                class-name="p-4 h-[500px] hover:scale-[103%] transition-all cursor-pointer"
+            />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import FeatureItem from "./FeatureItem.vue";
+
+defineProps({ listings: Array });
+</script>
