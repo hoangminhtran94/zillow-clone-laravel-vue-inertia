167daf21485ea4dae462951a6fd3c3f7495b368e
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-06-26 19:00:24 -0400

    Update website Title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <title>My App</title>
+    <title>Zillow Clone by Minh Hoang Tran</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
     @routes
     @vite('resources/js/app.js')
